8d16d7c8a66e1bde8ead14189d0ecdd6901f4f93
Valdielen Casarin <valcantarellic@gmail.com>
2024-12-12 13:21:35 -0500

    LeetCode75

--- src/interviewsPreparation/ReverseWords.java
+++ src/interviewsPreparation/ReverseWords.java
@@ -1,54 +1,29 @@
 package interviewsPreparation;
-
 // resolve espacos antes de retornar
-
 public class ReverseWords {
-
     public static void reverse(char[] sentence, int start, int end){
         int m = (end - start)/2;
         int j = end;
+        // First Step:
         for (int i = 0; i < m; i++){
             char temp = sentence[i];
             sentence[i] = sentence[j];
             sentence[j] = temp;
             j--;
         }
+        //Second Step: aplico o mesmo, mas cada palavra do array
+
+
+
+
+
     }
     public static void main(String[] args) {
-        String str = "hello       world  ";
+        String str = "  Casa pink ";
         char [] sentence = str.toCharArray();
         System.out.println(sentence);
         reverse(sentence,0, sentence.length-1);
         System.out.println(sentence);
-
-        int startPos = 0;
-        while (true){
-            int start = 0;
-            int end = 0;
-            int i =0;
-            //while (sentence[i] != ' '; i++){// quando sair daqui: teremos o start}
-            start = i;
-            Boolean letra = true;
-            while (letra){
-                i++;
-                //quando sair daqui: teremos o end
-            }
-            end = i -1;
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 
 }
