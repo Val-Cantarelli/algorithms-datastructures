b82bca2eaad4368bf692bc1767e89127033f91bc
Valdielen Casarin <valcantarellic@gmail.com>
2025-03-12 13:30:51 -0400

    LeetCode150Mar12

--- src/leetCode150/ValidAnagram.java
+++ src/leetCode150/ValidAnagram.java
@@ -4,17 +4,16 @@ import java.util.HashSet;
 import java.util.Set;
 
 /* EASY
-
-II first tried using an array to count frequencies, but started having problems tracking if I had used
+I first tried using an array to count frequencies, but started having problems tracking if I had used
 all the letters from 's' at the same frequency.
 So, in that case—without using a HashMap because we only have 26 letters—we are going to use a frequency
 array. We'll increment and decrement simultaneously, and in the end, check if all the 26 positions are equal to 0.
 */
 public class ValidAnagram {
     public static boolean isAnagram(String s, String t) {
-        int [] freq = new int[26];
         if (s.length() != t.length()) return false;
 
+        int [] freq = new int[26];
         int letterS, letterT;
         for(int i =0; i < s.length(); i++){
             letterS = s.charAt(i) - 97;
