0c846a83caaffb153fc0b194b66c1cb01f5a4524
Valdielen Casarin <valcantarellic@gmail.com>
2025-03-12 11:53:11 -0400

    LeetCode150Mar12

--- src/leetCode150/IsomorphicStrings.java
+++ src/leetCode150/IsomorphicStrings.java
@@ -5,6 +5,11 @@ Given two strings s and t, determine if they are isomorphic.
 Two strings s and t are isomorphic if the characters in s can be replaced to get t.
 All occurrences of a character must be replaced with another character while preserving the order of characters.
 No two characters may map to the same character, but a character may map to itself.
+
+=== Improvement ===
+The improvement replaces the `HashMap` with an `int[200]`, using characters as indices.
+This reduces `HashMap` overhead, improves direct access (O(1)), and saves memory,
+as long as the characters are ASCII
 */
 
 
@@ -23,7 +28,6 @@ public class IsomorphicStrings {
             value=t.charAt(i);
             // If the key does not exist and actual value already exist in usedValues: false
             if(!map.containsKey(key) && usedValues.contains(value))return false;
-
             // If the key exist and the actual value is diferent from what we got on map: return false
             if (map.containsKey(key) && !map.get(key).equals(value)) return false;
             map.put(key, value);
@@ -31,7 +35,6 @@ public class IsomorphicStrings {
         }
         return true;
     }
-
     public static void main(String[] args) {
         String s = "paper";
         String t = "title";
