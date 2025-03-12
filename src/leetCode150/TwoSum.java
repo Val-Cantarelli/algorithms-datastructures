bebe7dbbb870b312ef15d3305d095838a234caf6
Valdielen Casarin <valcantarellic@gmail.com>
2025-03-12 13:30:51 -0400

    LeetCode150Mar12

---
+++ src/leetCode150/TwoSum.java
@@ -0,0 +1,39 @@
+package leetCode150;
+/*Given an array of integers nums and an integer target, return indices of the two numbers
+such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not
+use the same element twice.
+
+You can return the answer in any order.*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        int complement,number=0;
+        for (int i = 0; i < nums.length; i++){
+            number = nums[i];
+            complement = target - number;
+
+            if(map.containsKey(complement)){
+                return new int[] { map.get(complement), i };
+            }
+            map.put(number,i);
+        }
+
+        return new int[0]; // The problem guarantee a solution
+    }
+
+    public static void main(String[] args) {
+        int []nums = {3,3};
+        int target = 6;
+        for(int elem:twoSum(nums,target)){System.out.print(elem);}
+
+
+    }
+
+}
