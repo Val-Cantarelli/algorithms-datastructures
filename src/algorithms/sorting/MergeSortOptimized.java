e153fd044a49e28f1083ef851148bee508402f00
Valdielen Casarin <valcantarellic@gmail.com>
2025-03-24 18:53:17 -0400

    MergeSort Optimization

---
+++ src/algorithms/sorting/MergeSortOptimized.java
@@ -0,0 +1,60 @@
+package algorithms.sorting;
+import java.util.Arrays;
+/*
+MergeSort classic optimization:
+Instead of copying both subarrays during the merge step, only the right half is copied to an auxiliary array.
+Then, the merge is performed in reverse order: starting from the end of both the left part (in the original array)
+and the right part (in the auxiliary array), moving backward while writing into the array from the end to the beginning.
+This avoids overwriting elements that haven't been merged yet and reduces memory allocation.
+*/
+public class MergeSortOptimized {
+    public static void sort(int[] arr) {
+        sort(arr, 0, arr.length-1);
+    }
+    public static void sort(int[] arr, int start, int end) {
+        if (end - start < 1) {
+            return;}
+
+        int middle = (start + end) / 2;
+
+        sort(arr, start, middle);
+        sort(arr, middle+1, end);
+
+        merge(arr, start, middle, end);
+    }
+
+    public static void merge(int[] arr, int start, int middle, int end) {
+
+        int[] aux = new int[end-middle];
+        System.arraycopy(arr, middle+1, aux, 0, aux.length);
+
+        int i = middle;
+        int j = aux.length - 1;
+        int k = end;
+
+
+        while (i >= start && j >= 0) {
+            if (arr[i] > aux[j]) {
+                arr[k] = arr[i];
+                i--;
+            } else {
+                arr[k] = aux[j];
+                j--;
+            }
+            k--;
+        }
+
+        while (i >= start) {
+            arr[k--] = arr[i--];
+        }
+
+        while (j >= 0) {
+            arr[k--] = aux[j--];
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = {38, 27, 43, 3,3};
+        sort(array);
+        System.out.println("Sorted array (Top-Down): " + Arrays.toString(array));
+    }
+}
--- src/algorithms/sorting/MergeSortTopDown.java
+++ src/algorithms/sorting/MergeSortTopDown.java
@@ -6,24 +6,18 @@ public class MergeSortTopDown {
         if (array.length <= 1) return;
 
         int mid = array.length / 2;
-        // Divide
+
         int[] left = Arrays.copyOfRange(array, 0, mid);
         int[] right = Arrays.copyOfRange(array, mid, array.length);
 
-       // order
         sort(left);
         sort(right);
 
-       //merge comparing
         merge(array, left,  right);
     }
 
     private static void merge(int[] array, int[] left, int[] right) {
         int i = 0, j = 0, k = 0;
-
-        // Combina os elementos dos subarrays em ordem
-        // OPTIMIZATION: copy only the smaller between left and rightand compares from right direction to left direction
-        //
         while (i < left.length && j < right.length) {
             if (left[i] <= right[j]) {
                 array[k++] = left[i++];
@@ -32,7 +26,6 @@ public class MergeSortTopDown {
             }
         }
 
-        // Adiciona os elementos restantes de cada subarray
         while (i < left.length) {
             array[k++] = left[i++];
         }
