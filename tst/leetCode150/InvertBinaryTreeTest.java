c94922269c352629b440ccc9d6c2141e9a18ca5d
Valdielen Casarin <valcantarellic@gmail.com>
2025-04-04 11:30:29 -0400

    LeetCode150 - Trees

---
+++ tst/leetCode150/InvertBinaryTreeTest.java
@@ -0,0 +1,26 @@
+package leetCode150;
+
+import interviewsPreparation.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static leetCode150.InvertBinaryTree.invertTree;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InvertBinaryTreeTest {
+
+    @Test
+    public void testInvertSimpleTree() {
+        SameTree.TreeNode root = new SameTree.TreeNode(2);
+        root.left = new SameTree.TreeNode(1);
+        root.right = new SameTree.TreeNode(3);
+
+        SameTree.TreeNode inverted = invertTree(root);
+
+        assertNotNull(inverted);
+
+        assertEquals(2, inverted.val);
+        assertEquals(3, inverted.left.val);
+        assertEquals(1, inverted.right.val);
+
+    }
+}
