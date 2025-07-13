9d1fd038b4cad0d6033855a6704706b8d2eeb00b
Val-Cantarelli <valcanterellic@gmail.com>
2025-07-12 20:43:12 -0400

    AdventsCode2022-day07

--- src/adventsCode/DirFile.java
+++ src/adventsCode/DirFile.java
@@ -1,8 +1,17 @@
 package adventsCode;
+/*
+ Polimorfismo: poso ter uma lista de DirFiles e chamar size() sem saber
+ se é um arquivo ou diretório
+*/
 
+// abstract: obriga as subclasses a fornecerem implementação de size
+public abstract class DirFile {
+    public final String name;
 
+    protected DirFile(String name) {
+        this.name = name;
+    }
 
-public class DirFile {
+    public abstract int size();
 
-    
 }
