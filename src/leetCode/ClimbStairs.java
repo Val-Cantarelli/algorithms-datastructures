82a36e7577c5788bf0c8977d369ad63028690472
Val Cantarelli <valcantarellic@gmail.com>
2025-10-06 15:32:40 -0400

    ClimbStairs and VowelStrings

--- src/leetCode/ClimbStairs.java
+++ src/leetCode/ClimbStairs.java
@@ -12,7 +12,6 @@ public class ClimbStairs {
     private int climb(int n, int[] memo) {
         if (n == 1) return 1;
         if (n == 2) return 2;
-        // se t
         if (memo[n] != -1) return memo[n];
         memo[n] = climb(n - 1, memo) + climb(n - 2, memo);
         return memo[n];
